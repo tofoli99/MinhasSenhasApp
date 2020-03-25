4ff05a0c5e4bd82ed038eed12b8e8db29e36e6f6
7700341207 <elcio.abrahao-ext@viavarejo.com.br>
2020-03-25 20:54:13 -0300

    Implementação do cadastro de usuário e da verificação parcial de senha via Hawk.

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroUsuarioActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroUsuarioActivity.java
@@ -3,12 +3,46 @@ package br.usjt.devmobile.minhassenhasapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.orhanobut.hawk.Hawk;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    private static final String TAG = "CadastroUsuarioActivity";
+    private TextInputEditText usuario;
+    private TextInputEditText senha;
+    private TextInputEditText pergunta;
+    private TextInputEditText resposta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+        usuario = (TextInputEditText)findViewById(R.id.usuarioEditTextInput);
+        senha = (TextInputEditText)findViewById(R.id.senhaEditTextInput);
+        pergunta = (TextInputEditText)findViewById(R.id.perguntaEditTextInput);
+        resposta = (TextInputEditText)findViewById(R.id.respostaEditTextInput);
+        Hawk.init(this).build();
     }
+
+    public void cadastrarUsuario(View view){
+        // TODO fazer o cadastro
+        Log.d(TAG,"Clicou no fazer cadastro!");
+
+        Hawk.put("usuario", usuario.getText().toString());
+        Hawk.put("senha", senha.getText().toString());
+        Hawk.put("pergunta", pergunta.getText().toString());
+        Hawk.put("resposta", resposta.getText().toString());
+
+        Toast.makeText(this,"Cadastro Realizado com Sucesso",Toast.LENGTH_LONG).show();
+
+        finish();
+
+    }
+
+
 }
--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/MainActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/MainActivity.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.orhanobut.hawk.Hawk;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Hawk.init(this).build();
     }
 
     public void fazerLogin(View view){
-        Intent intent = new Intent(this, ListaSenhasActivity.class);
-        startActivity(intent);
+
+        if(Hawk.contains("usuario")){
+            String usuario = Hawk.get("usuario");
+            Log.d(TAG,"Existe um usuário chamado: "+usuario);
+        }
+
+//        Intent intent = new Intent(this, ListaSenhasActivity.class);
+//        startActivity(intent);
     }
 
     public void novoCadastro(View view){
