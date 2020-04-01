9a1a24cf39e14e299b9102f51ddae5a4642b868b
2104499700 <elcio.abrahao@viavarejo.com.br>
2020-04-01 19:30:48 -0300

    Implementação do controle de senha de usuário e do controle de novo cadastro.

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/MainActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/MainActivity.java
@@ -6,33 +6,54 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.orhanobut.hawk.Hawk;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private TextInputEditText usuario;
+    private TextInputEditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Hawk.init(this).build();
+
+        usuario = findViewById(R.id.emailEditTextInput);
+        senha = findViewById(R.id.passwordEditTextInput);
+
     }
 
     public void fazerLogin(View view){
 
-        if(Hawk.contains("usuario")){
-            String usuario = Hawk.get("usuario");
-            Log.d(TAG,"Existe um usuário chamado: "+usuario);
+
+
+        if(usuario.getText().toString().equals(Hawk.get("usuario")) &&
+            senha.getText().toString().equals(Hawk.get("senha"))){
+
+            Intent intent = new Intent(this, ListaSenhasActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"Usuário ou senha incorretos!",Toast.LENGTH_SHORT).show();
         }
 
-//        Intent intent = new Intent(this, ListaSenhasActivity.class);
-//        startActivity(intent);
+
+
     }
 
     public void novoCadastro(View view){
-        Intent intent = new Intent(this, CadastroUsuarioActivity.class);
-        startActivity(intent);
+
+        if(Hawk.contains("usuario")){
+            Toast.makeText(this,"Usuário já cadastrado!",Toast.LENGTH_SHORT).show();
+        }else{
+            Intent intent = new Intent(this, CadastroUsuarioActivity.class);
+            startActivity(intent);
+        }
+
+
     }
 }
