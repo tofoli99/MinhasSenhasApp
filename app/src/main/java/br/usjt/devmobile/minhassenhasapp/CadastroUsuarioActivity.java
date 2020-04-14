1980071bfa2245ea6844095d8f3c9ea05ebc18b2
2104499700 <elcio.abrahao@viavarejo.com.br>
2020-04-13 21:35:42 -0300

    Implementação de Captura de imagem de usuário

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroUsuarioActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroUsuarioActivity.java
@@ -2,11 +2,15 @@ package br.usjt.devmobile.minhassenhasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.textfield.TextInputEditText;
 import com.orhanobut.hawk.Hawk;
 
@@ -44,5 +48,32 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
 
     }
 
+
+    public void capturarImagem(View view) {
+
+        ImagePicker.Companion.with(this)
+                .crop()
+                .compress(1024)
+                .maxResultSize(1080, 1080)
+                .start();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(RequestCode, ResultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            Uri fileUri = data.data;
+
+
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
 
 }
