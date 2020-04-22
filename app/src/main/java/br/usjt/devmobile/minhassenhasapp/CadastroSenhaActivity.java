9c3f90269d9bdf027d78e09e03143955f1a99595
2104499700 <elcio.abrahao@viavarejo.com.br>
2020-04-22 18:02:38 -0300

    Implementação de Alteração de Senha

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroSenhaActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroSenhaActivity.java
@@ -2,6 +2,8 @@ package br.usjt.devmobile.minhassenhasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
+
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +20,8 @@ public class CadastroSenhaActivity extends AppCompatActivity {
     private TextInputEditText url;
     private TextInputEditText observacao;
     private AppDatabase db;
+    private Senha senhaCorrente;
+    private boolean alteracao = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +34,44 @@ public class CadastroSenhaActivity extends AppCompatActivity {
         senha = (TextInputEditText)findViewById(R.id.cadSenhaEditTextInput);
         url = (TextInputEditText)findViewById(R.id.cadUrlEditTextInput);
         observacao = (TextInputEditText)findViewById(R.id.cadObservacaoEditTextInput);
+
+        Intent intent = getIntent();
+        if(intent !=null && intent.hasExtra("senha")){
+            alteracao = true;
+            senhaCorrente = (Senha)intent.getSerializableExtra("senha");
+            preencherCampos();
+        }
+    }
+
+    private void preencherCampos(){
+        nome.setText(senhaCorrente.getNome());
+        usuario.setText(senhaCorrente.getUsuario());
+        this.senha.setText(senhaCorrente.getSenha());
+        url.setText(senhaCorrente.getUrl());
+        observacao.setText(senhaCorrente.getObservacao());
     }
 
     public void cadastrarSenha(View view) {
 
-        final Senha senha = new Senha(
-                nome.getText().toString(),
-                usuario.getText().toString(),
-                this.senha.getText().toString(),
-                url.getText().toString(),
-                observacao.getText().toString()
-                );
+        if(alteracao){
+            senhaCorrente.setNome(nome.getText().toString());
+            senhaCorrente.setUsuario(usuario.getText().toString());
+            senhaCorrente.setSenha(this.senha.getText().toString());
+            senhaCorrente.setUrl(url.getText().toString());
+            senhaCorrente.setObservacao(observacao.getText().toString());
+            new AlterarSenhaAsyncTask().execute(senhaCorrente);
+        }else{
+            senhaCorrente = new Senha(
+                    nome.getText().toString(),
+                    usuario.getText().toString(),
+                    this.senha.getText().toString(),
+                    url.getText().toString(),
+                    observacao.getText().toString()
+            );
+            new SaveSenhaAsyncTask().execute(senhaCorrente);
+        }
 
-                new SaveSenhaAsyncTask().execute(senha);
+
     }
 
 
@@ -52,6 +81,21 @@ public class CadastroSenhaActivity extends AppCompatActivity {
         protected Boolean doInBackground(Senha... senha) {
            db.senhaDao().insertAll(senha);
            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean sucess){
+            Toast.makeText(CadastroSenhaActivity.this,"Senha criada com sucesso!",Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private class AlterarSenhaAsyncTask extends AsyncTask<Senha, Void, Boolean>
+    {
+        @Override
+        protected Boolean doInBackground(Senha... senha) {
+            db.senhaDao().updateSenha(senha[0]);
+            return true;
         }
 
         @Override
--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/DetalhesSenhaActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/DetalhesSenhaActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DetalhesSenhaActivity extends AppCompatActivity {
 
     private Senha senha;
     private TextView textViewSenha;
+    private TextView textViewUsuarioValue;
+    private TextView textViewSenhaValue;
+    private TextView textViewUrlValue;
+    private TextView textViewObsValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,39 @@ public class DetalhesSenhaActivity extends AppCompatActivity {
         Intent intent = getIntent();
         senha = (Senha)intent.getSerializableExtra("senha");
         textViewSenha = findViewById(R.id.textViewNomeValue);
+        textViewUsuarioValue = findViewById(R.id.textViewUsuarioValue);
+        textViewSenhaValue = findViewById(R.id.textViewSenhaValue);
+        textViewUrlValue = findViewById(R.id.textViewUrlValue);
+        textViewObsValue = findViewById(R.id.textViewObsValue);
+
         textViewSenha.setText(senha.getNome());
+        textViewUsuarioValue.setText(senha.getUsuario());
+        textViewSenhaValue.setText(senha.getSenha());
+        textViewUrlValue.setText(senha.getUrl());
+        textViewObsValue.setText(senha.getObservacao());
+    }
+
+
+    public void fecharActivity(View view) {
+        finish();
+    }
+
+    public void alterarSenha(View view) {
+
+        senha.setNome(textViewSenha.getText().toString());
+        senha.setUsuario(textViewUsuarioValue.getText().toString());
+        senha.setSenha(textViewSenhaValue.getText().toString());
+        senha.setUrl(textViewUrlValue.getText().toString());
+        senha.setObservacao(textViewObsValue.getText().toString());
+
+        Intent intent = new Intent(this, CadastroSenhaActivity.class);
+        intent.putExtra("senha",senha);
+        startActivity(intent);
+        finish();
+    }
+
+    public void deletarSenha(View view) {
+        // Mostrar aqui um dialog e confirmar a deleção da senha
+        finish();
     }
 }
